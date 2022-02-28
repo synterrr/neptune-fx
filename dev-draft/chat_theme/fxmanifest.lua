cd62850ed62be6b74242584c08a150ae81e55089
neptunium-dev <47131318+douth-c@users.noreply.github.com>
2022-02-28 00:34:56 -0600

    [dev-draft] chat theme

---
+++ dev-draft/chat_theme/fxmanifest.lua
@@ -0,0 +1,17 @@
+-- Custom resource made by Douth for Neptune-Dev.
+
+game 'common'
+version '1.0.0'
+author 'douth-c'
+fx_version 'cerulean'
+description 'Custom chat theme for Neptune-Dev.'
+
+dependency "chat"
+
+chat_theme "chat-theme" {
+    styleSheet = "chat-theme.css"
+}
+
+files {
+    "chat-theme.css",
+}
