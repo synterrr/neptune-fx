8bd1a61ecce97da4eff5641ac441ba575823c4c2
neptunium-dev <47131318+douth-c@users.noreply.github.com>
2022-03-01 13:55:53 -0600

    [fx-assets] creation + dlc_map

---
+++ resources/[fx-assets]/dlc_pausemap/fxmanifest.lua
@@ -0,0 +1,10 @@
+-- We did not create the data, ref. README.
+-- Custom resource made by Douth for Neptune-Dev.
+
+game 'common'
+version '1.0.0'
+author 'douth-c'
+fx_version 'cerulean'
+description 'Custom map theme for Neptune-Dev.'
+
+client_script 'client/mapzoom.lua'
