0e8ef5bb9b1f0b4a7c054fd5b09a6b1daad54d09
neptunium-dev <47131318+douth-c@users.noreply.github.com>
2022-03-02 23:46:51 -0600

    new framework draft

---
+++ resources/[framework]/os_invt/fxmanifest.lua
@@ -0,0 +1,13 @@
+game 'common'
+version '1.0.0'
+fx_version 'cerulean'
+author 'douth-c & synter'
+description 'Resource built for Neptune Open Source.'
+
+shared_scripts {
+    'settings.lua',
+    'shared/shared.lua'
+}
+
+server_script 'server/server.lua'
+client_script 'client/client.lua'
